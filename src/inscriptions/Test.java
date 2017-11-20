0a94991dc81e4ff235a694e19d47ed5d70856605
lyauy <32457055+lyauy@users.noreply.github.com>
2017-11-20 10:44:44 +0100

    modification TODO et début de test unitaire

---
+++ src/inscriptions/Test.java
@@ -0,0 +1,24 @@
+package inscriptions;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestInscriptions {
+
+	@Test
+	public void testcreateCompetition() {
+		Inscriptions.createCompetition("test", , true );
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testcreatePersonne() {
+		Inscriptions.createPersonne("test","test2","test3" );
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testcreateEquipe() {
+		Inscriptions.createEquipe("test", , true );
+		fail("Not yet implemented");
+	}
+}
