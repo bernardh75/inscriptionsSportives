12c60b4397640697187d006b33fa0c7f3d268d43
bernardh <bernardh@10.188.177.171>
2018-04-22 17:54:13 +0200

    Hibernate + class fenêtre

---
+++ src/ihmis/javaswingis.java
@@ -0,0 +1,38 @@
+package ihmis;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class javaswingis extends JFrame {
+	  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public javaswingis(){
+	    this.setTitle("InscriptionsSportives M2L");
+	    //dimensions
+	    this.setSize(400, 500);
+	    //au centre
+	    this.setLocationRelativeTo(null);
+	    //boutonrougeclose
+	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+	    //premier plan tj
+	    this.setAlwaysOnTop(true);
+	    //redimensionnable
+	    this.setResizable(true);
+	    //Instanciation d'un objet JPanel
+	    JPanel pan = new JPanel();
+	    //Définition de sa couleur de fond
+	    pan.setBackground(Color.CYAN);        
+	    //On prévient notre JFrame que notre JPanel sera son content pane
+	    this.setContentPane(pan);
+	    //visibilité de la fenêtre on
+	    this.setVisible(true);
+	    
+	  }
+  public static void main(String[] args){
+	  javaswingis fen = new javaswingis();
+  }       
+}
